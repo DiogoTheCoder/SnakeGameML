1b177d63a6f9ec0ade56460b9f4c7614cf1b53fc
Diogo Costa <hello@diogothecoder.com>
2018-09-30 10:33:22 +0100

    Enabled multiprocessing, game will run multiple times based on CPU logical core count

--- SnakeGameML/SnakeGameML.py
+++ SnakeGameML/SnakeGameML.py
@@ -1,13 +1,27 @@
 from Game import Game
 import SnakeBot
 import mysql.connector
+from multiprocessing import Process
+import os
 
-if __name__ == "__main__":
+def runGame():
     cnx = mysql.connector.connect(user='root', password='admin',
-                              host='104.199.13.110',
-                              database='snakegame')
-
+                            host='104.199.13.110',
+                            database='snakegame')
     app = Game(cnx)
     app.on_execute()
-    #SnakeBot.randy()
+
     cnx.close()
+
+if __name__ == "__main__":
+    # Multiprocessing
+    processes = []
+
+    for x in range(os.cpu_count()):
+        processes.append(Process(target=runGame))
+
+    for process in processes:
+        process.start()
+
+    for process in processes:
+        process.join()
